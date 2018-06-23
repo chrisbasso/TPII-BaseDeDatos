b76e768fea1a8c17caf9d8055c61330ff92a7f64
Christian Basso <christian.d.basso@gmail.com>
2018-06-23 07:31:50 +0000

    Unificado los datos para que los cargue en una sola instrucción

---
+++ datos.sql
@@ -0,0 +1,66 @@
+insert into cliente values(27944, 'Pedro', 'Gallego', 'Colon 1798', '134694659957');
+insert into cliente values(55783, 'Hugo', 'Pascual', 'Benito Juarez 2744', '933452593086');
+insert into cliente values(99572, 'Gabriel', 'Diez', 'Bolivar 668', '420825613829');
+insert into cliente values(12448, 'Marcos', 'Romero', 'Pellegrini 148', '954479437556');
+insert into cliente values(85976, 'Daniel', 'Alvarez', 'Pinamar 148', '767870189949');
+insert into cliente values(67870, 'Cristian', 'Castro', 'La Costa 2281', '731149524073');
+insert into cliente values(21976, 'Jesus', 'Garcia', 'Lincoln 182', '137573370047');
+insert into cliente values(46697, 'Adria', 'Sanz', 'Maipu 5782', '348882793834');
+insert into cliente values(32736, 'Silvia', 'Arias', 'Rivadavia 217', '669847370538');
+insert into cliente values(66920, 'Lidia', 'Duran', 'Pilar 2562', '341855578140');
+insert into cliente values(40062, 'Ana', 'Perez', 'Dolores 866', '967900864219');
+insert into cliente values(21390, 'Lara', 'Torres', 'Escobar 4876', '168702849958');
+insert into cliente values(13439, 'Laia', 'Dominguez', 'Ezeiza 5652', '693846396768');
+insert into cliente values(36365, 'Leire', 'Velasco', 'General Paz 1033', '391883222325');
+insert into cliente values(97565, 'Alejandra', 'Perez', 'Hipolito Yrigoyen 153', '224121439626');
+insert into cliente values(65477, 'Rocio', 'Ramirez', 'Hurlingham 2896', '539584927901');
+insert into cliente values(86202, 'Leyre', 'Calvo', 'Moron 486', '421371882169');
+insert into cliente values(27336, 'Helena', 'Vazquez', 'San Miguel 3455', '213600643550');
+insert into cliente values(54976, 'Daniela', 'Cano', 'Quilmes 784', '396958609377');
+insert into cliente values(00001, 'Lucas', 'Lugo', 'Calle Falsa 123', '123456789101');
+
+insert into comercio values(3050, 'ShopBox', 'Avenida Corrientes 1530', '54335459', '822554406952');
+insert into comercio values(4040, 'MC Donalds', 'Avenida Cordoba 1556', '19397229', '897845638311');
+insert into comercio values(2782, 'Kentucky Fried Chicken', 'Otamendi 565', '91279858', '343326353960');
+insert into comercio values(2852, 'Garbarino', 'Jose de la Quintana 4512', '95395837', '740027834627');
+insert into comercio values(1425, 'Compumundo', 'Clemente Diez de Medina 841', '60503282', '986155314132');
+insert into comercio values(9805, 'Fravega', 'Pueyrredon 3265', '90043609', '962800442018');
+insert into comercio values(9376, 'Falabella', 'Balcarce 253', '29143552', '809818312657');
+insert into comercio values(3381, 'Xtreme', 'Almafuerte 1234', '88481270', '804177716195');
+insert into comercio values(5685, 'The Soul Of The Clothes', 'Lugones 703', '84402031', '828959664492');
+insert into comercio values(6882, 'Kirolak', '25 de Mayo 5012', '33933114', '230221110765');
+insert into comercio values(9604, 'Tutti Frutti', 'Campos Salles 4567', '54335459', '261237403404');
+insert into comercio values(2890, 'CopyArt', 'Corrientes 2415', '19397229', '476509541985');
+insert into comercio values(5069, 'Farmacia Mateos', 'El Sereno 2832', '91279858', '590547282356');
+insert into comercio values(9970, 'Olivia The Shop', '5 de Julio 7894', '95395837', '562907175732');
+insert into comercio values(7518, 'Odisea', 'Florida 1766', '60503282', '584501565653');
+insert into comercio values(2415, 'Wendys', 'Gallardo 1845', '90043609', '916232162226');
+insert into comercio values(2678, 'Supermercado Estrella', 'Uriburu 4192', '29143552', '809837492845');
+insert into comercio values(5150, 'Carniceria El Mago', 'Jorge Newbery 3859', '88481270', '701618154473');
+insert into comercio values(2563, 'Replay', 'Enrique de Vedia 2021', '84402031', '272065387484');
+insert into comercio values(8315, 'Legacy', 'Talavera 2145', '33933114', '987890423781');
+
+insert into tarjeta values(475913199634, 27944, '201704', '201904', '2516', 420000.00, 'vigente');
+insert into tarjeta values(489419235332, 55783, '201607', '201907', '5820', 380000.00, 'vigente');
+insert into tarjeta values(426422689520, 99572, '201710', '201910', '7913', 275000.00, 'vigente');
+insert into tarjeta values(415349622351, 12448, '201701', '201901', '0358', 550000.00, 'vigente');
+insert into tarjeta values(453360446991, 85976, '201707', '202007', '1835', 532000.00, 'vigente');
+insert into tarjeta values(470171791311, 67870, '201801', '202001', '4872', 350000.00, 'vigente');
+insert into tarjeta values(419694811672, 21976, '201604', '201904', '6821', 470000.00, 'vigente');
+insert into tarjeta values(436782605294, 46697, '201711', '201911', '3064', 580000.00, 'vigente');
+insert into tarjeta values(466575225837, 32736, '201612', '201912', '6760', 430000.00, 'vigente');
+insert into tarjeta values(463934309504, 66920, '201702', '202002', '6281', 574000.00, 'vigente');
+insert into tarjeta values(481906032085, 40062, '201802', '202002', '2598', 384000.00, 'vigente');
+insert into tarjeta values(492082306601, 21390, '201704', '201904', '2984', 632000.00, 'vigente');
+insert into tarjeta values(479110600471, 13439, '201708', '201908', '6524', 340000.00, 'vigente');
+insert into tarjeta values(485834874942, 36365, '201607', '201907', '1505', 500000.00, 'vigente');
+insert into tarjeta values(437434819262, 97565, '201605', '201905', '9076', 480000.00, 'vigente');
+insert into tarjeta values(400877394804, 65477, '201702', '202002', '3791', 490000.00, 'vigente');
+insert into tarjeta values(488207236937, 86202, '201705', '201905', '2650', 350000.00, 'vigente');
+insert into tarjeta values(437501035853, 27336, '201512', '201712', '8764', 380000.00, 'anulada');
+insert into tarjeta values(498539870963, 54976, '201710', '202010', '3951', 299000.00, 'vigente'); -- de Daniela Cano
+insert into tarjeta values(426914694871, 54976, '201611', '201911', '7035', 349000.00, 'vigente'); -- de Daniela Cano
+insert into tarjeta values(457105551649, 00001, '201612', '201912', '1904', 600000.00, 'vigente'); -- de Lucas Lugo
+insert into tarjeta values(425311796515, 00001, '201802', '202102', '7361', 300000.00, 'vigente'); -- de Lucas Lugo
+
+
