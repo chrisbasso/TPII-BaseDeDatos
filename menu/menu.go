64f064f7d0ffa2ab9e93aa0bc5549a04c5ad741a
Christian Basso <christian.d.basso@gmail.com>
2018-06-27 16:55:00 +0000

    menu - no funcionando

---
+++ menu/menu.go
@@ -0,0 +1,45 @@
+package menu
+
+import (
+
+	"fmt"
+
+)
+func main(){
+	
+}
+
+
+func mostrarMenu(){
+
+
+
+	fmt.Println("")
+	fmt.Println("             \x1b[32;1m  --BASE DE DATOS TARJETAS--\x1b[0m")
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Println("  * * * * * * * * * * * * * * * * * * * * * * * * * * *")
+	fmt.Println("  *                                                   *")
+	fmt.Println("  *  \x1b[33;1m01 - CREAR TABLAS\x1b[0m                                *")
+	fmt.Println("  *  \x1b[33;1m02 - ESTABLECER PKs y FKs\x1b[0m                        *")
+	fmt.Println("  *  \x1b[33;1m03 - CARGAR FUNCIONES\x1b[0m                            *")
+	fmt.Println("  *  \x1b[33;1m04 - CARGAR DATOS\x1b[0m                                *")
+	fmt.Println("  *  \x1b[33;1m05 - MOSTRAR TABLAS\x1b[0m                              *")
+	fmt.Println("  *  \x1b[33;1m06 - MOSTRAR CLIENTES\x1b[0m                            *")
+	fmt.Println("  *  \x1b[33;1m07 - MOSTRAR TARJETAS\x1b[0m                            *")
+	fmt.Println("  *  \x1b[33;1m08 - MOSTRAR COMERCIOS\x1b[0m                           *")
+	fmt.Println("  *  \x1b[33;1m09 - MOSTRAR COMPRAS\x1b[0m                             *")
+	fmt.Println("  *  \x1b[33;1m10 - MOSTRAR RECHAZOS\x1b[0m                            *")
+	fmt.Println("  *  \x1b[33;1m11 - REALIZAR COMPRA AUTORIZADA\x1b[0m                  *")
+	fmt.Println("  *  \x1b[33;1m12 - REALIZAR COMPRA CON TARJETA ANULADA\x1b[0m         *")
+	fmt.Println("  *  \x1b[33;1m13 - REALIZAR COMPRA CON TARJETA SUSPENDIDA\x1b[0m      *")
+	fmt.Println("  *  \x1b[33;1m14 - REALIZAR COMPRA SUPERANDO EL LIMITE\x1b[0m         *")
+	fmt.Println("  *  \x1b[33;1m15 - SALIR\x1b[0m                                       *")
+	fmt.Println("  *                                                   *")
+	fmt.Println("  * * * * * * * * * * * * * * * * * * * * * * * * * * *")
+	fmt.Println("")
+	fmt.Println("   ELIJA UNA OPCIÓN")
+	fmt.Println("----------------------------------------------------------------------------------------")
+
+
+}
