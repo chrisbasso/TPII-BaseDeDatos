10dd0dd8a128e40890b922bfedea7e14eaca1e4b
Christian Basso <christian.d.basso@gmail.com>
2018-06-28 04:38:36 +0000

    Menu para app CLI JSON

---
+++ menuJSON/menu.go
@@ -0,0 +1,31 @@
+package menu
+
+import (
+
+	"fmt"
+
+)
+
+func MostrarMenu(){
+
+
+
+	fmt.Println("")
+	fmt.Println("             \x1b[32;1m      --BASE DE DATOS TARJETAS--\x1b[0m")
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Println("  * * * * * * * * * * * * * * * * * * * * * * ")
+	fmt.Println("  *                                         * ")
+	fmt.Println("  *  \x1b[33;1m01 - CARGAR Y MOSTRAR CLIENTES\x1b[0m         *")
+	fmt.Println("  *  \x1b[33;1m02 - CARGAR Y MOSTRAR TARJETAS\x1b[0m         *")
+	fmt.Println("  *  \x1b[33;1m03 - CARGAR Y MOSTRAR COMERCIOS\x1b[0m        *")
+	fmt.Println("  *  \x1b[33;1m04 - CARGAR Y MOSTRAR COMPRAS\x1b[0m          *")
+	fmt.Println("  *  \x1b[33;1m05 - SALIR\x1b[0m                             *")
+	fmt.Println("  *                                         *")
+	fmt.Println("  * * * * * * * * * * * * * * * * * * * * * *")
+	fmt.Println("")
+	fmt.Println("   ELIJA UNA OPCIÓN")
+	fmt.Println("-------------------------------------------------------------------------------")
+
+
+}
